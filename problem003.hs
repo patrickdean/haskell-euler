ea8f34a947b28f5b4635d277f70d21827af841cf
Patrick Dean <patrick.l.dean@gmail.com>
2014-05-18 01:15:03 -0500

    Refactor problem #3

--- problem003.hs
+++ problem003.hs
@@ -1,11 +1,9 @@
-largestFactor n 1 = floor n
-largestFactor n x = if isIntValue (x / n) then 
-                largestFactor n (x / n)
+largestFactor :: (Integral a) => a -> a -> a
+largestFactor n 1 = n
+largestFactor n x = if x `mod` n == 0 then 
+                largestFactor n (x `quot` n)
              else 
                 largestFactor (n + 1) x
-
-
-isIntValue x = x == (fromInteger $ floor x)
 
 answer = largestFactor 2 600851475143  
 
